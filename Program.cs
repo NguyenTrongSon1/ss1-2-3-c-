a4f3b9dc47b48f91aa2515aae2013f57e439bc3a
NguyenTrongSon1 <65001546+NguyenTrongSon1@users.noreply.github.com>
2020-12-26 21:12:21 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int doMolt = 5 * 20;
+            int doDiv = doMolt / 2;
+            Console.WriteLine("Quotient is: " + doDiv);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
